182b46e25500e2cf184473f6806c5bf1ed9bb28e
Ivan Schuetz <ivanschuetz@gmail.com>
2025-09-22 06:17:25 +0200

    remove outlined text

--- composeApp/src/commonMain/kotlin/foo/bar/inputtestdemo/App.kt
+++ composeApp/src/commonMain/kotlin/foo/bar/inputtestdemo/App.kt
@@ -10,13 +10,11 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
@@ -57,25 +55,10 @@ fun App() {
                     Text("Main content area")
                 }
 
-//                OutlinedText()
                 BasicText()
             }
         }
     }
-}
-
-@Composable
-private fun OutlinedText() {
-    var textValue by remember { mutableStateOf("") }
-
-    OutlinedTextField(
-        value = textValue,
-        onValueChange = { textValue = it },
-        placeholder = { Text("Enter text...") },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(bottom = 20.dp), // ignored
-    )
 }
 
 @Composable
