8f4879776caa30c59f79ede81437ae7480e9318a
Ivan Schuetz <ivanschuetz@gmail.com>
2025-10-08 09:12:57 +0200

    Adjust inner padding based on keyboard's visibility
    https://youtrack.jetbrains.com/issue/CMP-9010/Layout-issues-opening-keyboard#focus=Comments-27-12763248.0-0

--- composeApp/src/commonMain/kotlin/foo/bar/inputtestdemo/App.kt
+++ composeApp/src/commonMain/kotlin/foo/bar/inputtestdemo/App.kt
@@ -4,9 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.text.BasicTextField
@@ -16,14 +19,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.unit.dp
@@ -42,13 +48,30 @@ fun App() {
         bottomBar = { TabsBar() }
     ) { innerPadding ->
 
+        val padding = if (WindowInsets.isImeVisible) {
+            PaddingValues(0.dp)
+        } else {
+            innerPadding
+        }
+
 //      Box(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.padding(innerPadding)) {
+        Box(modifier = Modifier.padding(padding)) {
 //      Box {
             Contents()
         }
     }
 }
+
+val WindowInsets.Companion.isImeVisible: Boolean
+    @Composable
+    get() {
+        val density = LocalDensity.current
+        val ime = ime
+
+        return remember(density) {
+            derivedStateOf { ime.getBottom(density) > 0 }
+        }.value
+    }
 
 @Composable
 private fun Contents() {
